3c2595f643d1749bcc93c9ac2bbbae6794d4370e
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-22 06:41:30 +0200

    basic layout

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
-  return <article>Hello</article>;
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>Okaaay Let's Go</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
+  );
 };
