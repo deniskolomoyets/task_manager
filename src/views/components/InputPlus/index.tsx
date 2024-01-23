30eb0d06f0f5ec4a92fc81c42d403e67c9572786
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-01-23 06:57:28 +0200

    add logic for component InputPlus

--- src/views/components/InputPlus/index.tsx
+++ src/views/components/InputPlus/index.tsx
@@ -5,5 +5,33 @@ interface InputePlusProps {
   onAdd: (title: string) => void;
 }
 export const InputPlus: React.FC<InputePlusProps> = ({ onAdd }) => {
-  return <div>Wowow</div>;
+  const [inputValue, setInputValue] = React.useState("");
+
+  const addTask = React.useCallback(() => {
+    onAdd(inputValue);
+    setInputValue("");
+  }, [inputValue]); //usecallback is used so that a new function is not created during re-rendering
+
+  return (
+    <div className={styles.inputPlus}>
+      <input
+        type="text"
+        className={styles.inputPlusValue}
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addTask();
+          }
+        }}
+      />
+      <button
+        onClick={addTask}
+        aria-label="Add"
+        className={styles.inputPlusButton}
+      />
+    </div>
+  );
 };
